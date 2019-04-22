4167d532b1cbf0042ceaf745af444e14c515c70b
Siddhant043 <43211861+Siddhant043@users.noreply.github.com>
2019-04-22 16:17:03 +0530

    Diamond in rectangle added

--- program.cpp
+++ program.cpp
@@ -1,5 +1,54 @@
 #include<iostream>
 using namespace std;
+pattern()                                            //Diamond in rectangle
+{
+    int i,j;
+    int n=7;
+
+    // This is upper half of pattern
+    for (i=1; i<=n; i++)
+    {
+        for (j=1; j<=(2*n); j++)
+        {
+            // Left part of pattern
+            if (i>(n-j+1))
+                cout<<" ";
+            else
+                cout<<"*";
+
+            // Right part of pattern
+            if ((i+n)>j)
+                cout<<" ";
+            else
+                cout<<"*";
+        }
+        cout<<endl;;
+    }
+
+    // This is lower half of pattern
+    for (i=1; i<=n; i++)
+    {
+        for (j=1; j<=(2*n); j++)
+        {
+            // Right Part of pattern
+            if (i<j)
+                cout<<" ";
+            else
+                cout<<"*";
+
+            // Left Part of pattern
+            if (i<=((2*n)-j))
+                cout<<" ";
+            else
+                cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+/*-------------------------------------------------*/
+
+
 
 int floydstri(){                            //Floyd's Triangle
 int rows, number = 1;
@@ -287,7 +336,7 @@ int main()
              int option=-1;
              while(option)
                {
-                 printf("1.Half Pyramid using *  \n2.Half Pyramid a using Numbers\n3.Half pyramid using Alphabets\n4.Inverted Half Pyramid using *\n5.Inverted half pyramid using *\n6.Inverted half pyramid using numbers\n7.Full pyramid using *\n8.Pyramid Using numbers\n9.Inverted full pyramid using *\n10.Pascal's Triangle\n11.Floyd's Triangle\n12.Exit\n");
+                 printf("1.Half Pyramid using *  \n2.Half Pyramid a using Numbers\n3.Half pyramid using Alphabets\n4.Inverted Half Pyramid using *\n5.Inverted half pyramid using *\n6.Inverted half pyramid using numbers\n7.Full pyramid using *\n8.Pyramid Using numbers\n9.Inverted full pyramid using *\n10.Pascal's Triangle\n11.Floyd's Triangle\n12.Diamond in rectangle\n13.Exit\n");
                  printf("Enter your choice :- ");
                  scanf("%d",&choice);
                  switch(choice)
@@ -313,6 +362,8 @@ int main()
                    case 10: pascaltri();
                             break;
                    case 11: floydstri();
+                            break;
+                   case 12: pattern();
                             break;
                  }
                  fflush(stdin);
