f8e39330ee966991127b86bf20db746174d6575c
Siddhant043 <43211861+Siddhant043@users.noreply.github.com>
2019-04-22 16:05:54 +0530

    Floyd's Triangle

--- program.cpp
+++ program.cpp
@@ -1,5 +1,30 @@
 #include<iostream>
 using namespace std;
+
+int floydstri(){                            //Floyd's Triangle
+int rows, number = 1;
+
+  cout << "Enter number of rows: ";
+  cin >> rows;
+
+  for(int i = 1; i <= rows; i++)
+  {
+      for(int j = 1; j <= i; ++j)
+      {
+          cout << number << " ";
+          ++number;
+      }
+
+      cout << endl;
+  }
+
+  return 0;
+}
+
+
+/*---------------------------------------------------*/
+
+
 int pascaltri(){                           //Pascal's triangle
 int rows, number = 1;
 
@@ -262,7 +287,7 @@ int main()
              int option=-1;
              while(option)
                {
-                 printf("1.Half Pyramid using *  \n2.Half Pyramid a using Numbers\n3.Half pyramid using Alphabets\n4.Inverted Half Pyramid using *\n5.Inverted half pyramid using *\n6.Inverted half pyramid using numbers\n7.Full pyramid using *\n8.Pyramid Using numbers\n9.Inverted full pyramid using *\n10.Pascal's Triangle\n11.Exit\n");
+                 printf("1.Half Pyramid using *  \n2.Half Pyramid a using Numbers\n3.Half pyramid using Alphabets\n4.Inverted Half Pyramid using *\n5.Inverted half pyramid using *\n6.Inverted half pyramid using numbers\n7.Full pyramid using *\n8.Pyramid Using numbers\n9.Inverted full pyramid using *\n10.Pascal's Triangle\n11.Floyd's Triangle\n12.Exit\n");
                  printf("Enter your choice :- ");
                  scanf("%d",&choice);
                  switch(choice)
@@ -286,6 +311,8 @@ int main()
                    case 9: ifusings();
                            break;
                    case 10: pascaltri();
+                            break;
+                   case 11: floydstri();
                             break;
                  }
                  fflush(stdin);
