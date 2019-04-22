077c59c123ef50d4b26a82db740a1a796f9f75e9
Ma9si <46700948+Ma9si@users.noreply.github.com>
2019-04-22 15:35:36 +0530

    Pascal's triangle

--- program.cpp
+++ program.cpp
@@ -1,5 +1,165 @@
 #include<iostream>
 using namespace std;
+int pascaltri(){                           //Pascal's triangle
+int rows, number = 1;
+
+   cout << "Enter number of rows: ";
+   cin >> rows;
+
+   for(int i = 1; i <= rows; i++)
+   {
+       for(int j = 1; j <= i; ++j)
+       {
+           cout << number << " ";
+           ++number;
+       }
+
+       cout << endl;
+   }
+
+   return 0;
+
+return 0;
+}
+
+
+/*------------------------------------------------------------------*/
+
+
+
+int ifusings(){                             //Full inverted pyramid using *
+  int rows;
+
+   cout << "Enter number of rows: ";
+   cin >> rows;
+
+   for(int i = rows; i >= 1; --i)
+   {
+       for(int space = 0; space < rows-i; ++space)
+           cout << "  ";
+
+       for(int j = i; j <= 2*i-1; ++j)
+           cout << "* ";
+
+       for(int j = 0; j < i-1; ++j)
+           cout << "* ";
+
+       cout << endl;
+   }
+
+   return 0;
+}
+
+
+/*-----------------------------------------------------------*/
+
+
+int punum(){                                //pyramid using numbers
+  int rows, count = 0, count1 = 0, k = 0;
+
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+    for(int i = 1; i <= rows; ++i)
+    {
+        for(int space = 1; space <= rows-i; ++space)
+        {
+            cout << "  ";
+            ++count;
+        }
+
+        while(k != 2*i-1)
+        {
+            if (count <= rows-1)
+            {
+                cout << i+k << " ";
+                ++count;
+            }
+            else
+            {
+                ++count1;
+                cout << i+k-2*count1 << " ";
+            }
+            ++k;
+        }
+        count1 = count = k = 0;
+
+        cout << endl;
+    }
+    return 0;
+}
+
+
+/*-------------------------------------------------------------*/
+
+
+int fullpstar(){                                 //Program to print full pyramid using *
+  int space, rows;
+
+    cout <<"Enter number of rows: ";
+    cin >> rows;
+
+    for(int i = 1, k = 0; i <= rows; ++i, k = 0)
+    {
+        for(space = 1; space <= rows-i; ++space)
+        {
+            cout <<"  ";
+        }
+
+        while(k != 2*i-1)
+        {
+            cout << "* ";
+            ++k;
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+
+/*--------------------------------------------------------*/
+
+
+int invhnum(){                                   //Inverted half pyramid using numbers
+  int rows;
+
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+    for(int i = rows; i >= 1; --i)
+    {
+        for(int j = 1; j <= i; ++j)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
+
+/*---------------------------------------------------------------*/
+
+
+int invhpystar(){                                 //Inverted half pyramid using *
+    int rows;
+
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+    for(int i = rows; i >= 1; --i)
+    {
+        for(int j = 1; j <= i; ++j)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+
+/*---------------------------------------------------------------*/
+
+
+
 int ihpyramid()                                   //inverted half pyramid using *
 {
     int rows;
@@ -15,7 +175,7 @@ int ihpyramid()                                   //inverted half pyramid using 
         }
         cout << endl;
     }
-    
+
     return 0;
 }
 
@@ -101,7 +261,7 @@ int main()
              int option=-1;
              while(option)
                {
-                 printf("1.Half Pyramid using * , 2.Half Pyramid a using Numbers , 3.Half pyramid using Alphabets , 4.Inverted Half Pyramid using *, 5.Exit\n");
+                 printf("1.Half Pyramid using *  \n2.Half Pyramid a using Numbers\n3.Half pyramid using Alphabets\n4.Inverted Half Pyramid using *\n5.Inverted half pyramid using *\n6.Inverted half pyramid using numbers\n7.Full pyramid using *\n8.Pyramid Using numbers\n9.Inverted full pyramid using *\n10.Pascal's Triangle\n11.Exit\n");
                  printf("Enter your choice :- ");
                  scanf("%d",&choice);
                  switch(choice)
@@ -114,6 +274,18 @@ int main()
                            break;
                    case 4: ihpyramid();
                            break;
+                   case 5: invhpystar();
+                           break;
+                   case 6: invhnum();
+                           break;
+                   case 7: fullpstar();
+                           break;
+                   case 8: punum();
+                           break;
+                   case 9: ifusings();
+                           break;
+                   case 10: pascaltri();
+                            break;
                  }
                  fflush(stdin);
                  printf("To continue press 1\nTo exit press 0.\n -  ");
